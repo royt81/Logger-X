62b8faadc148897d62d385f614a16aaa574d1b03
Roy Tate <107740928+royt81@users.noreply.github.com>
2024-05-02 20:05:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function run() {
     phoneticList.id = 'phoneticList';
     const dFA = document.createElement('img');
     dFA.id = 'dFA';
-    dFA.src = './favicon/DFA1.png?v=1';
+    dFA.src = './favicon/DFA2.png?v=1';
     phoneticList.appendChild(dFA);
     contact.appendChild(phoneticList);
   })
